8456d2d10f38ffd73315229b961f1818a22ea9ca
i.kharlamenko <i.kharlamenko@1440.space>
2024-12-21 12:26:20 +0300

    Project preparing

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    std::cout << "Hello from awesome cmake console application! :-)" << std::endl;
-
-    return 0;
-}
--- tests/console_app_tests.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(console_app_tests, placeholder_test)
-{
-    EXPECT_EQ(0, 1);
-}
